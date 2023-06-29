703160fbcc859c9a8f028729771aff107c6fc7b8
Miguel Nunes <miguel.npa@gmail.com>
2023-06-29 14:58:57 -0300

    cap2

---
+++ Cap2.java
@@ -0,0 +1,32 @@
+// // Esse arquivo tem como finalidade responder os exercícios do livro Introdução 
+// // à programação com Python, porém na linguagem Java, para fins de aprendizado. 
+
+// public class Cap2 {
+//     public static void main(String[] arg) {
+
+        // // Exercício 2.4
+
+        // // Escreva um programa que exiba o resultado de 2a × 3b, em que a vale 3 e b vale 5.
+
+        // // RESPOSTA 
+
+        // int a = 3;
+        // int b = 5;
+        // int c = (2 * a) * (3 * b);
+        // System.out.println(c);
+
+        // // Exercício 2.6
+
+        // // calcular um aumento de 15% para um salário de R$ 750.
+
+        // // RESPOSTA
+
+        // int salario = 750;
+        // double aumento = 15.0/100;
+
+        // double salarioFinal = (salario * aumento) + salario;
+
+        // System.out.println(salarioFinal);
+
+//     }
+// }
---
+++ Cap3.java
@@ -0,0 +1,8 @@
+// // Esse arquivo tem como finalidade responder os exercícios do livro Introdução 
+// // à programação com Python, porém na linguagem Java, para fins de aprendizado. 
+
+public class Cap3 {
+    static void main(String[] args) {
+        
+    }
+}
