36874930a3eb8b0ffb167a587d03740ed68369f3
Miguel Nunes <miguel.npa@gmail.com>
2023-06-30 09:46:26 -0300

    atualização 3 capítulo

--- Cap3.java
+++ Cap3.java
@@ -1,8 +1,83 @@
 // // Esse arquivo tem como finalidade responder os exercícios do livro Introdução 
 // // à programação com Python, porém na linguagem Java, para fins de aprendizado. 
 
+import java.util.Scanner;
+
 public class Cap3 {
-    static void main(String[] args) {
+    public static void main(String[] args) {
+
+        //  // Exercício 3.4
+
+        // // Escreva uma expressão para determinar se uma pessoa deve ou não pagar imposto. 
+        // // Considere que pagam imposto pessoas cujo salári~o é maior que R$ 1.200,00.
+
+        // // RESPOSTA 
+
+        // double salario = 1500.00;
+
+        // if (salario > 1200.00) {
+        //     System.out.println("Deve pagar imposto");
+        // } else {
+        //     System.out.println("Não deve pagar imposto");
+        // }
+
+        // // EXERCÍCIO 03.06
+
+        // //  Escreva uma expressão que será utilizada para decidir se um aluno foi ou não aprovado. 
+        // //  Para ser aprovado, todas as médias do aluno devem ser maiores que 7. 
+        // //  Considere que o aluno cursa apenas três matérias, e que a nota de cada uma está armazenada nas seguintes variáveis: matéria1, matéria2 e matéria3.
+        
+        // // RESPOSTA
+
+        // double materia1 = 8;
+        // double materia2 = 7;
+        // double materia3 = 9;
+        
+        // if (materia1 >= 7 && materia2 >=7 && materia3 >=7 ) {
+        //     System.out.println("Aprovado");
+        // } else {
+        //     System.out.println("Reprovado");
+        // }
+        
+        // // EXERCÍCIO 03-07 
+        // // Faça um programa que peça dois números inteiros.  Imprima a soma desses dois números na tela.
+        
+        // // RESPOSTA
+        
+        // Scanner input = new Scanner(System.in);
+
+        // System.out.println("Digite um número inteiro: ");
+
+        // int inteiro1 = Integer.parseInt(input.next());
+
+        // System.out.println("Digite outro número inteiro: ");
+
+        // int inteiro2 = Integer.parseInt(input.next());
+        
+        // System.out.println("A soma desses números é: %d".formatted(inteiro1 + inteiro2));
+
+        // input.close(); 
+
+        // // EXERCÍCIO 03-08 
+
+        // // Escreva um programa que leia um valor em metro e o exiba convertido em mílimetros.
+
+        // // RESPOSTA
+
+        // Scanner input = new Scanner(System.in);
+
+        // System.out.print("Digite o valor em metros: ");
+        
+        // String entradaUsuario = input.next();
+
+        // double entradaUsuarioConvertida = Double.parseDouble(entradaUsuario);
+
+        // double valorEmMilimetros = entradaUsuarioConvertida * 1000;
+
+        // System.out.println("%.2f metros equivalem a %.2f milímetros".formatted(entradaUsuarioConvertida,valorEmMilimetros));
+
+        // input.close();
+        
         
     }
 }
